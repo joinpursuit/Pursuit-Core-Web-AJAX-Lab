10a860394135cfdbaae2f4d41c3b4da5d4264bc0
kadjiah <kadijahwilson@pursuit.org>
2019-09-15 17:51:10 -0400

    Fetch Lab

--- ajax.js
+++ ajax.js
@@ -11,9 +11,11 @@ function getRandomImage() {
             let response = JSON.parse(this.response);
             console.log(response)
             console.log(response.message)
+            let imgSrc = response.message;
+            fetchDogImage(imgSrc);
 
-           let image = document.querySelector("img");
-           image.src = response.message;
+        //    let image = document.querySelector("img");
+        //    image.src = response.message;
         }
     }
     xhr.open("GET", "https://dog.ceo/api/breeds/image/random");
@@ -21,3 +23,18 @@ function getRandomImage() {
     console.log(xhr);
 }
 
+const fetchDogImage = (url) => {
+let img = document.querySelector('img');
+// img.src = url
+if(!img) {
+    let dogImg = document.createElement('img');
+     dogImg.src = url
+    document.body.appendChild(dogImg);
+}
+else{
+    // img.src = url
+    let newDogImg = document.createElement('img')
+    newDogImg.src = url;
+    img.parentNode.replaceChild(newDogImg, img)
+}
+} 
